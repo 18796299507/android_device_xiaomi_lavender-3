eb53347f2263a242a037cfaa83b4734bcf94f4d5
sYYLG <932961454@qq.com>
2019-08-14 10:15:48 +0800

    lavender: Import thermal from ...

--- device.mk
+++ device.mk
@@ -426,7 +426,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
-    thermal.sdm660
+    thermal.lavender
 
 # Touchscreen
 PRODUCT_PACKAGES += \
