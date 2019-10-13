PixelExperience-Devices/device_xiaomi_lavender

lavender: Create root folders outside of vendor init script
Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

BoardConfig.mk
@@ -201,6 +201,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_VENDORIMAGE_PARTITION_SIZE := 2147483648 # 2097152 * 1024
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_ROOT_EXTRA_FOLDERS := cust
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /mnt/vendor/persist:/persist \
     /vendor/dsp:/dsp \

18796299507/android_device_xiaomi_lavender-3

 * The core SELinux policies for vendor_init are being restricted as to
   comply with Treble. Simply adding a local rule to allow creating
   folders under rootfs would be enough, but at the same time defining
   the extra root folders and making use of AID/fs configuration file
   to set perms is more neat and clean.
 * Furthermore, with System As Root / is a read-only filesystem
   so we can't create our mountpoints from init script anymore
Signed-off-by: sYYLG <932961454@qq.com>

BoardConfig.mk
@@ -208,6 +208,7 @@ TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 TARGET_USES_MKE2FS := true
 
 TARGET_COPY_OUT_VENDOR := vendor
+BOARD_ROOT_EXTRA_FOLDERS := persist
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"